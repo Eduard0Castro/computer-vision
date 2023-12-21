b78898d60b5047ebe88f305c095567738d996aad
Eduardo Castro <americogomes1@outlook.com>
2023-12-21 18:41:49 -0300

    Images Processing class adaptation

---
+++ Projects/src/ImagesProcessing.hpp
@@ -0,0 +1,160 @@
+#ifndef IMAGESPROCESSING_HPP
+#define IMAGESPROCESSING_HPP
+#include <iostream>
+#include <opencv2/opencv.hpp>
+ 
+using namespace std;
+
+class ImagesProcessing{
+    protected:
+
+        cv::Mat copy;
+        int rows, cols;
+
+
+    public:
+
+        ImagesProcessing(cv::Mat img){
+            
+            copy = img;
+            rows = copy.rows;
+            cols = copy.cols;
+
+        }
+
+        ~ImagesProcessing(){
+
+        }
+
+        cv::Mat negative();
+        cv::Mat resize();
+        cv::Mat swap();
+        cv::Mat faceDetection();
+        void cut();
+};
+
+cv::Mat ImagesProcessing::negative(){
+    
+    cv::Mat neg = copy.clone();
+    int coordenadas[4] = {};
+
+    cout << endl;
+
+    do{
+        cout << "Digite o x da coordenada de início: ";
+        cin >> coordenadas[0];
+    }
+    while(coordenadas[0] < 0 || coordenadas[0] > cols);
+
+    do{
+        cout << "Digite o y da coordenada de início: ";
+        cin >> coordenadas[1];
+    }
+    while(coordenadas[1] < 0 || coordenadas[1] > rows);
+
+    do{
+        cout << "Digite o x da coordenada final: ";
+        cin >> coordenadas[2];
+    }
+    while(coordenadas[2] < 0 || coordenadas[2] > cols);
+
+    do{
+        cout << "Digite o y da coordenada final: ";
+        cin >> coordenadas[3];
+    }
+    while (coordenadas[3] < 0 || coordenadas[3] > rows);
+
+    for (int i = coordenadas[0]; i <= coordenadas[2]; i++){
+        for (int j = coordenadas[1]; j < coordenadas[3]; j++){
+            neg.at<cv::Vec3b>(j,i)[0] = 255 - copy.at<cv::Vec3b>(j,i)[0];
+            neg.at<cv::Vec3b>(j,i)[1] = 255 - copy.at<cv::Vec3b>(j,i)[1];
+            neg.at<cv::Vec3b>(j,i)[2] = 255 - copy.at<cv::Vec3b>(j,i)[2];
+        }
+    }
+
+    return neg;
+}
+
+cv::Mat ImagesProcessing::resize(){
+
+    cv::Mat resized;
+    int width, height;
+
+    cout << endl;
+
+    do{
+        cout << "Digite o novo valor para a altura: ";
+        cin >> height;
+    }
+    while(height <=0 || height > 3*copy.size().height);
+
+    do{
+        cout << "Digite o novo valor para a largura: ";
+        cin >> width;
+    }
+    while(width <= 0 || width > 3*copy.size().width);
+
+    cv::resize(copy, resized, cv::Size(width, height));
+
+    return resized;
+}
+
+cv::Mat ImagesProcessing::swap(){
+    
+    cv::Mat3b swapped(rows, cols, cv::Vec3b(0,0,0));
+
+    cv::Rect rois[4] = {
+        cv::Rect(0, 0, cols/2, rows/2), 
+        cv::Rect(cols/2, 0, cols/2, rows/2),
+        cv::Rect(0, rows/2, cols/2, rows/2), 
+        cv::Rect(cols/2, rows/2, cols/2, rows/2) 
+    };
+
+    int sequence [4] = {3,1,2,0};
+
+    for (int i = 0; i < 4; i++){
+        cv::Mat region(copy, rois[i]);
+        region.copyTo(swapped(rois[sequence[i]]));
+    }
+
+    return swapped;
+
+}
+
+cv::Mat ImagesProcessing::faceDetection(){
+   
+    cv::Mat detections = copy.clone();
+    cv::String haar_cascade = "../Projects/XML/haarcascade_frontalface_default.xml";
+    cv::CascadeClassifier detect;
+    vector<cv::Rect> detects;
+    
+
+    detect.load(haar_cascade);
+    if (detect.empty()){
+        cerr << "Não foi possível carregar o XML" << endl;
+    }
+
+    detect.detectMultiScale(copy, detects);
+
+    for (int i = 0; i < detects.size(); i++){
+        cv::rectangle(detections, detects[i].tl(), detects[i].br(), cv::Scalar(0, 255, 0), 2);
+    }
+
+    return detections;
+
+}
+
+void ImagesProcessing::cut(){
+
+    cv::Mat cortes;
+    int n_cortes = 5;
+
+    for (int i = 0; i < n_cortes; i++){
+        cortes = copy(cv::Rect(0, rows*i/n_cortes, 
+                      copy.size().width, copy.size().height/n_cortes));
+        cv::imwrite(cv::format("../Projects/Images/eneas%d.jpg", i+1), cortes);
+    }
+}
+
+
+#endif
--- Projects/src/main.cpp
+++ Projects/src/main.cpp
@@ -1,34 +1,22 @@
-#include<iostream>
-#include <opencv2/opencv.hpp>
-
-using namespace std;
-
-cv::Mat negative(cv::Mat);
-cv::Mat resize(cv::Mat);
-cv::Mat swap(cv::Mat);
-void cut(cv::Mat);
-cv::Mat face_detection(cv::Mat);
+#include "ImagesProcessing.hpp"
 
 int main(){
 
-
     cv::String path = "../Projects/Images/eneas.jpg";
     cv::Mat eneas = cv::imread(path), neg, resized, swapped, faces;
-
+    ImagesProcessing teste(eneas);
     if (eneas.empty()){
         cerr << "Erro ao carregar imagem\n";
         return -1;
     }
 
-    
+    neg = teste.negative();
+    resized = teste.resize();
+    swapped = teste.swap();
+    faces = teste.faceDetection();
+    teste.cut();
 
-    neg = negative(eneas);    
-    resized = resize(eneas);
-    swapped = swap(eneas);
-    cut(eneas);
-    faces = face_detection(eneas);
-    
-    //cv::bitwise_not(eneas, neg); função para negativar toda a imagem direto
+    // //cv::bitwise_not(eneas, neg); função para negativar toda a imagem direto
     cv::imwrite("../Projects/Images/eneas_resized.jpg", resized );
     cv::imshow("Nosso nome é Enéas negativo", neg);
     cv::imshow("Redimensionada: ", resized);
@@ -40,142 +28,4 @@ int main(){
 
     return 0;
 
-}
-
-cv:: Mat negative(cv::Mat img){
-    
-    cv::Mat neg;
-    int coordenada[4] = {};
-    int rows = img.rows;
-    int cols = img.cols;
-
-    neg = img.clone(); //Cria uma cópia independente
-
-    cout << rows <<  " x " << cols << endl;
-    cout << endl;
-        
-    do{
-        cout << "Digite o x da primeira coordenada da imagem: ";
-        cin >> coordenada[0];
-    }
-    while (coordenada [0] < 0 || coordenada[0] > rows);
-
-    do{
-        cout << "Digite o y da primeira coordenada da imagem: ";
-        cin >> coordenada[1];
-    }
-    while (coordenada[1] < 0 || coordenada[1] > cols);
-
-    do{
-        cout << "Digite o x da segunda coordenada da imagem: ";
-        cin >> coordenada[2];
-    }
-    while(coordenada[2] < 0 || coordenada[2] > rows);
-
-    do{
-        cout << "Digite o y da segunda coordenada da imagem: ";
-        cin >> coordenada[3];
-    }
-    while (coordenada[3] < 0 || coordenada [3] > cols);
-
-    //Para o óculos Dr. Eneas: 131 x 80 ; 198 x 258
-
-
-    for (int i = coordenada[0]; i < coordenada[2]; i++){
-        for (int j = coordenada[1]; j < coordenada[3]; j++){
-            neg.at<cv::Vec3b>(i, j)[0] = 255 - img.at<cv::Vec3b>(i, j)[0];
-            neg.at<cv::Vec3b>(i, j)[1] = 255 - img.at<cv::Vec3b>(i, j)[1];
-            neg.at<cv::Vec3b>(i, j)[2] = 255 - img.at<cv::Vec3b>(i, j)[2];
-        }
-    }
-
-    return neg;
-}
-
-cv::Mat resize(cv::Mat img){
-    cv::Mat resized;
-    int width, height;
-    int rows = img.rows, cols = img.cols;
-
-    resized = img.clone();
-    cout << rows << " x " << cols << endl;
-
-
-    do{
-    cout << "Digite um valor para a nova altura: ";
-    cin >> height;
-    }
-    while(height <= 0 || height > 3*rows);
-    
-    do{
-    cout << "Digite um valor para a nova largura: ";
-    cin >> width;
-    }
-    while(width <= 0 || width > 3*cols);
-
-    cv::resize(img,resized, cv::Size(width, height));
-
-    return resized;
-}
-
-cv::Mat swap(cv::Mat img){
-
-
-    int rows = img.rows, cols = img.cols;
-    cv::Mat3b swapped(rows, cols, cv::Vec3b(0,0,0));
-
-    cv::Rect rois[4] = {
-      
-        cv::Rect(0, 0, cols/2, rows/2),
-        cv::Rect(cols/2, 0, cols/2, rows/2),
-        cv::Rect(0, rows/2, cols/2, rows/2),
-        cv::Rect(cols/2, rows/2, cols/2, rows/2)
-
-    };
-
-    int sequence[4] = {3,1,2,0};
-    
-    for (int i = 0; i <= 3; i++){
-     
-        cv::Mat region (img, rois[i]);
-        region.copyTo(swapped(rois[sequence[i]]));
-    }
-
-    return swapped;
-}
-
-void cut(cv::Mat img){
-
-    cv::Mat copia = img.clone();
-    int rows = copia.rows, cols = copia.cols;
-    cv::Mat cortes;
-    int n_cortes = 5;
-
-    for (int i = 0; i < n_cortes; i++){
-      
-        cortes = copia(cv::Rect(0, rows*i/n_cortes, cols, rows/n_cortes));
-        cv::imwrite(cv::format("../Projects/Images/eneas%d.jpg", i+1), cortes);
-    }
-
-}
-
-cv::Mat face_detection(cv::Mat img){
-    
-    cv::String haar_cascade = "../Projects/XML/haarcascade_frontalface_default.xml";
-    vector<cv::Rect> detects;
-    cv::CascadeClassifier detect;
-    detect.load(haar_cascade);
-    cv::Mat detections = img.clone();
-
-    if (detect.empty()){
-        cerr << "Não possível carregar o arquivo .XML" << endl;
-    }
-
-    detect.detectMultiScale(img, detects);
-
-    for (int i = 0; i < detects.size();i++){
-        cv::rectangle(detections, detects[i].tl(), detects[i].br(), cv::Scalar(0, 255, 0), 2);  
-    }
-
-    return detections;
 }
