64b01b6ea9ddb77c73daca745c68ca3acce4ac77
Eduardo Castro <americogomes1@outlook.com>
2024-01-15 14:52:59 -0300

    Cube perspective tests

--- Projects/src/Python/Segmentation/canny_coffee.py
+++ Projects/src/Python/Segmentation/canny_coffee.py
@@ -9,7 +9,8 @@ gray = cv2.cvtColor(coffee, cv2.COLOR_BGR2GRAY)
 # Só para mostrar que é possível:
 binary = gray.copy()
 cv2.threshold(gray, 135, 255, cv2.THRESH_BINARY_INV, binary)
-cv2.morphologyEx(binary, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT, (3,3)), binary)
+cv2.morphologyEx(binary, cv2.MORPH_CLOSE, 
+                 cv2.getStructuringElement(cv2.MORPH_RECT, (3,3)), binary)
 canny = cv2.Canny(binary, 100, 200)
 
 cv2.imshow("Coffee", coffee)
